f4cfc6904f9a52a0d75ed8307fe96dff069b3969
brtastic <brtastic.dev@gmail.com>
2021-05-23 21:11:35 +0200

    Metaobject example from documentation

---
+++ examples/metaobject.pl
@@ -0,0 +1,36 @@
+use v5.10;
+use warnings;
+use Test::More;
+
+{
+	package MetaForm;
+
+	# Moo for easy role mixing and a constructor
+	use Moo;
+
+	# gives us create_form_meta function
+	use Form::Tiny::Utils qw(:meta_handlers);
+
+	# we need this role mixed in
+	with qw(Form::Tiny::Form);
+
+	# meta roles go into the qw()
+	create_form_meta(__PACKAGE__, qw());
+
+	# add a requried field
+	__PACKAGE__->form_meta->add_field('field-name' => (
+		required => 1,
+	));
+
+	1;
+}
+
+my $form = MetaForm->new(
+	input => {
+		'field-name' => 42,
+	}
+);
+
+ok($form->valid, "no DSL form works");
+
+done_testing();
